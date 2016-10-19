7ed1dcd617e64b5ae7ad760bccb06be90818dadd
Laurence <C14526257@mydit.ie>
2016-10-19 16:16:30 +0100

    lab 5

--- dt228_cloud/app.py
+++ dt228_cloud/app.py
@@ -5,5 +5,25 @@ app = Flask(__name__)
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route("/")
+@app.route('/post/80')
+def post():
+    return 'Post 80'
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route("/")
+@app.route('/hello')
+def hellow():
+    return 'Hello'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0,port=8080', debug=True)
